045f26c72a396c01f4da9e59a7820ed57c008a89
ajaygatla1234 <149674849+ajaygatla1234@users.noreply.github.com>
2024-06-01 16:46:07 +0530

    Add files via upload

---
+++ Collection Framework/Comparable & Comparator/Comparable_Demo.java
@@ -0,0 +1,139 @@
+package com.gl.comparablecomparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+class Student implements Comparable<Student>{
+
+	int id;
+	String name;
+	String city;
+	float salary;
+
+	public Student() {
+
+	}
+
+	public Student(int id, String name, String city, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + ", salary=" + salary + "]";
+	}
+	
+	//Sorting with respect to id
+	@Override
+	public int compareTo(Student student) {
+	    // Compare based on ID
+	    if (this.getId() < student.getId()) {
+	        return -1;
+	    } else if (this.getId() > student.getId()) {
+	        return 1;
+	    } else {
+	        return 0;
+	    }
+	}//using Functional Programming(lambda Expression) will make easy to override compareTo() in Comparable interface
+
+	
+	//Sorting with respect to Name
+	@Override
+	public int compareTo(Student student) {     //this compareTo(Student o) is from Comparable interface
+		if(this.getName().compareTo(student.getName())>0) {  //this compareTo(Student o) is from String class
+			return 1;
+		}
+		else if(this.getName().compareTo(student.getName())<0) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}//using Functional Programming(lambda Expression) will make easy to override compareTo() in Comparable interface
+
+	/*
+	 * In the String class in Java, the compareTo() method is used to compare one string to another lexicographically. 
+	 * This means it compares strings based on their Unicode values, character by character.
+	 */
+
+}
+
+public class Comparable_Demo {
+
+	public static void main(String[] args) {
+
+		ArrayList<Student> students=new ArrayList<Student>();
+
+		students.add(new Student(5,"Subbu","Hyderabad", 25000));
+		students.add(new Student(4,"Navin","Coimabtore", 25000));
+		students.add(new Student(3,"Jaggu","Pune", 25000));
+		students.add(new Student(2,"Rakesh","Mumbai", 25000));
+		students.add(new Student(1,"Prashanth","Banglore", 25000));
+
+		System.out.println("Before Sorting");
+		Iterator<Student> stuItr=students.iterator();
+		while(stuItr.hasNext()) {
+			System.out.println(stuItr.next());
+		}
+		
+		/*
+		 * When you call Collections.sort(students), the compareTo method of the Student class is invoked multiple times during the sorting process. 
+		 * This method compares pairs of Student objects to determine their relative ordering.
+		 * The sort method in the Collections class uses a variation of Timsort. 
+		 * Timsort is a hybrid sorting algorithm derived from merge sort and insertion sort, and it is designed to perform well on many kinds of real-world data. 
+		 */
+		Collections.sort(students);  //
+		
+		System.out.println("\nAfter Sorting");
+		Iterator<Student> stuItr1=students.iterator();
+		while(stuItr1.hasNext()) {
+			
+			Student student = stuItr1.next(); // Retrieve the next student
+		    // Print the attributes of the student
+		    System.out.print(student.getName()+" : ");
+		    System.out.print(student.getCity()+" : ");
+		    System.out.print(student.getId());
+		    System.out.println();
+		}
+		
+	}
+}
---
+++ Collection Framework/Comparable & Comparator/Comparator_Demo.java
@@ -0,0 +1,155 @@
+package com.gl.comparablecomparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
+class Customer{
+
+	int id;
+	String name;
+	String city;
+	float purchaseValue;
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(int id, String name, String city, float purchaseValue) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.purchaseValue = purchaseValue;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public float getPurchaseValue() {
+		return purchaseValue;
+	}
+
+	public void setPurchaseValue(float purchaseValue) {
+		this.purchaseValue = purchaseValue;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", city=" + city + ", purchaseValue=" + purchaseValue + "]";
+	}
+}
+
+class CustomerIdSorter implements Comparator<Customer>{
+
+	@Override
+	public int compare(Customer customer1, Customer customer2) {
+
+		if(customer1.getId()>customer2.getId()) {
+			return 1;
+		}
+		else if(customer1.getId()<customer2.getId()){
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+}
+
+class CustomerCitySorter implements Comparator<Customer>{
+
+	@Override
+	public int compare(Customer o1, Customer o2) {
+		if(o1.getCity().compareTo(o2.getCity())>0) {
+			return 1;
+		}
+		else if(o1.getCity().compareTo(o2.getCity())<0){
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+}
+
+class CustomerPurchaseValueSorter implements Comparator<Customer>{
+
+	@Override
+	public int compare(Customer o1, Customer o2) {
+
+		if(o1.getPurchaseValue()>o2.getPurchaseValue()) {
+			return 1;
+		}
+		else if(o1.getPurchaseValue()<o2.getPurchaseValue()) {
+			return -11;
+		}
+		else {
+			return 0;
+		}
+	}
+
+}
+
+public class Comparator_Demo {
+
+	public static void main(String[] args) {
+
+		ArrayList<Customer> customers=new ArrayList<Customer>();
+
+		customers.add(new Customer(5, "Subbu", "Hyderabad", 6546));
+		customers.add(new Customer(4, "Navin", "Pune", 456));
+		customers.add(new Customer(3, "Rakesh", "Banglore", 65456));
+		customers.add(new Customer(2, "Jaggu", "Chennai", 45566));
+		customers.add(new Customer(1, "Prashanth", "Mumbai", 654653466));
+
+		System.out.println("----------Original Data----------");
+		Iterator<Customer> cusItr1=customers.iterator();
+		while(cusItr1.hasNext()) {
+			System.out.println(cusItr1.next());
+		}
+
+		System.out.println("\n----------Sorting based on Id----------");
+		Collections.sort(customers, new CustomerIdSorter());
+		Iterator<Customer> cusItr2=customers.iterator();
+		while(cusItr2.hasNext()) {
+			System.out.println(cusItr2.next());
+		}
+
+		System.out.println("\n----------Sorting based on City----------");
+		Collections.sort(customers, new CustomerCitySorter());
+		Iterator<Customer> cusItr3=customers.iterator();
+		while(cusItr3.hasNext()) {
+			System.out.println(cusItr3.next());
+		}
+
+		System.out.println("\n----------Sorting based on Purchase Value----------");
+		Collections.sort(customers, new CustomerPurchaseValueSorter());
+		Iterator<Customer> cusItr4=customers.iterator();
+		while(cusItr4.hasNext()) {
+			System.out.println(cusItr4.next());
+		}
+	}
+}
